31a2c7cc7966caaff355af8dde78a0a0ad001183
helalubo <alejandrodemoraiz@gmail.com>
2020-10-01 00:00:35 -0300

    tp2 laboratorio II faltan comentarios

---
+++ tp2/Entidades/Sedan.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+   public class Sedan : Vehiculo
+    {
+        private ETipo tipo;
+
+
+
+        /// <summary>
+        /// Por defecto, TIPO será Monovolumen
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color)
+            :base(chasis, marca, color)
+        {
+            this.tipo = ETipo.CincoPuertas;
+        }
+
+        public Sedan(EMarca marca, string chasis, ConsoleColor color, ETipo tipo) : this(marca, chasis, color)
+        {
+
+        }
+
+        /// <summary>
+        /// Los automoviles son medianos
+        /// </summary>
+        public override ETamanio Tamanio
+        {
+            get
+            {
+           
+                return ETamanio.Mediano;
+            
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SEDAN");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine($"TIPO : {this.tipo}" );
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+           public enum ETipo { 
+            
+            CuatroPuertas, CincoPuertas 
+        
+            }
+    }
+}
---
+++ tp2/Entidades/Vehiculo.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Vehiculo
+    {
+
+        private string chasis;
+        private ConsoleColor color;
+        private EMarca marca;
+
+
+        /// <summary>
+        /// ReadOnly: Retornará el tamaño
+        /// </summary>
+        public abstract ETamanio Tamanio 
+        { 
+            
+            get;
+                
+                
+         }
+
+
+
+
+        public Vehiculo(string chasis, EMarca marca, ConsoleColor color)
+        {
+            this.chasis = chasis;
+            this.marca = marca;
+            this.color = color;
+        }
+
+        
+        /// <summary>
+        /// Publica todos los datos del Vehiculo.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            string rta = (string)this;
+
+            return rta;
+
+           
+        }
+
+        public static explicit operator string(Vehiculo vehiculo)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append( $"CHASIS: { vehiculo.chasis}\r\n");
+            sb.Append( $"MARCA : {vehiculo.marca}\r\n");
+            sb.Append($"COLOR : {vehiculo.color.ToString()}\r\n");
+            sb.Append("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos vehiculos son iguales si comparten el mismo chasis
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            return (v1.chasis == v2.chasis);
+        }
+        /// <summary>
+        /// Dos vehiculos son distintos si su chasis es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+
+
+
+            return !(v1.chasis == v2.chasis);
+        }
+
+         public enum EMarca
+        {
+            Chevrolet, Ford, Renault, Toyota, BMW, Honda, HarleyDavidson
+        }
+      public  enum ETamanio
+        {
+            Chico, Mediano, Grande
+        }
+      
+
+    }
+}
